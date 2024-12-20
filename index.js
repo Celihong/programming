39dcaf0d398e6f86c7a6e3200ddd00dd326b8f59
Sokhon Senghong <sokhon.senghong@institute.pse.ngo>
2024-12-20 16:07:36 +0700

    add answer

--- index.js
+++ index.js
@@ -2,33 +2,48 @@
 // ===========================>>Follow the instructions for each task and complete the code.<<=====================================
 
 // Task 1: Comment Your JavaScript Code
-// Write a single-line comment below this line:
+// Write a single-line comment below this line
+comment in javaScript are used to explain the code in javaScript :
 
 // Task 2: Declare JavaScript Variables
-// Declare a variable named `myName`:
+// Declare a variable named `myName`
+ var myName ="hong";
 
 // Task 3: Storing Values with the Assignment Operator
-// Assign the value "JavaScript" to a variable named `myLanguage`:
+// Assign the value "JavaScript" to a variable named `myLanguage`
+ var myLanguage;
+ myLanguage = "JavaScript";
 
 // Task 4: Assigning the Value of One Variable to Another
-// Declare a variable named `a` with the value 5, and another variable `b`. Assign `a` to `b`:
+// Declare a variable named `a` with the value 5, and another variable `b`. Assign `a` to `b`
+ var a ;
+ a = 5 ;
+ var b ;
+a = b ;
 
 // Task 5: Initializing Variables with the Assignment Operator
-// Declare and initialize a variable named `myAge` with the value 25:
+// Declare and initialize a variable named `myAge` with the value 25
+ var myAge = 25;
 
 // Task 6: Declare String Variables
-// Declare a string variable named `greeting` and set it to "Hello, World!":
+// Declare a string variable named `greeting` and set it to "Hello, World!" var greeting = "Hello, World!":
 
 // Task 7: Understanding Uninitialized Variables
-// Declare three variables `x`, `y`, and `z` without assigning values. Then, assign values 1, 2, and 3 to them:
+// Declare three variables `x`, `y`, and `z` without assigning values. Then, assign values 1, 2, and 3 to them
+ var x = 1;
+ var y = 2;
+ var z = 3;
 
 // Task 8: Understanding Case Sensitivity in Variables
 // Fix the variable names to follow case sensitivity:
 // let StUdEnTnAmE = "John";
 // let sTuDeNtAgE = 20;
+ var studentName = "John";
+var studentAge = " 20";
 
 // Task 9: Explore Differences Between the var and let Keywords
 // Declare a variable using `var` and another using `let`. Show the difference in scope (use a block of code to demonstrate):
 
 // Task 10: Declare a Read-Only Variable with the const Keyword
-// Declare a constant variable `PI` and assign it the value 3.14:
+// Declare a constant variable `PI` and assign it the value 3.14
+ const PT = 3.14; 
